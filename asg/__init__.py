5650d7a1e17726c7fb11463a0f880dac1bdc6590
geltz <ethiquelia@gmail.com>
2025-10-23 12:06:43 -0400

    Add files via upload

--- asg/__init__.py
+++ asg/__init__.py
@@ -202,7 +202,7 @@ class AttentionShuffleGuidanceModelPatch:
             "required": {
                 "model": ("MODEL",),
                 "strength": ("FLOAT", {"default": 0.25, "min": 0.0, "max": 2.0, "step": 0.05}),
-                "rescale": ("FLOAT", {"default": 0.5, "min": 0.0, "max": 1.0, "step": 0.01}),
+                "rescale": ("FLOAT", {"default": 0.5, "min": 0.0, "max": 1.0, "step": 0.05}),
                 "seed": ("INT", {"default": 42, "min": 0, "max": 0xffffffffffffffff}),
             }
         }
